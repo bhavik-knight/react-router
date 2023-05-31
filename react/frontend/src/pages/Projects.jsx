fdec3157cc8d9cf15a7a3ae0b5fdec1cb30f8a0b
Bhavik <15388512+bhavik-knight@users.noreply.github.com15388512+bhavik-knight@users.noreply.github.com>
2023-05-31 20:07:19 -0300

    added some packages for fetching data from website using API calls

--- react/frontend/src/pages/Projects.jsx
+++ react/frontend/src/pages/Projects.jsx
@@ -1,10 +1,37 @@
-import React from 'react'
+import axios from "axios";
+import { nanoid } from "nanoid";
+import { useState, useEffect } from "react";
+import { Carousel } from "react-responsive-carousel";
+import "./Projects.css";
 
 function Projects() {
+  const [data, setData] = useState([]);
+  const transformedData = data.map((p) => {
     return (
-        <div>Projects</div>
-    )
+      <div
+        key={nanoid()}
+        style={{ border: "1px solid", borderRadius: "1em", margin: "2em", width: "70%", padding: "2em" }}
+      >
+        <h3>{p.title}</h3>
+        <p>{p.description}</p>
+        <img src={p.thumbnail} />
+      </div>
+    );
+  });
+
+  useEffect(() => {
+    async function getData() {
+      const response = await axios.get("https://dummyjson.com/products/");
+      setData(response.data.products);
+    }
+    getData();
+  }, []);
+
+  return (
+    <div>
+      {transformedData}
+    </div>
+  )
 }
 
-
-export { Projects }
+export { Projects };
