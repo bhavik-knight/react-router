074957ba056d56bd9f89bc747558c77f8f1ae864
Bhavik <15388512+bhavik-knight@users.noreply.github.com15388512+bhavik-knight@users.noreply.github.com>
2023-06-02 00:18:08 -0300

    added auto increment timer button in skills

--- react/frontend/src/pages/Skills.jsx
+++ react/frontend/src/pages/Skills.jsx
@@ -1,8 +1,23 @@
-import React from 'react'
+import { useRef, useState, useCallback, useEffect } from 'react'
+
 
 function Skills() {
+    const [count, setCount] = useState(0)
+
+    const handleDec = () => setCount(count => count - 1)
+    const handleInc = () => setCount(count => count + 1)
+
+    useEffect(() => {
+        // call increment at 5 seconds interval
+        setTimeout(() => handleInc(), 5000)
+    }, [count])
+
     return (
-        <div>Skills</div>
+        <div style={{ display: "flex", alignItems: "center" }}>
+            <button onClick={handleDec}>-</button>
+            <div style={{ margin: "2em" }}>{count}</div>
+            <button onClick={handleInc}>+</button>
+        </div>
     )
 }
 
