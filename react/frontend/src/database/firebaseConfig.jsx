6b2e9f4f86ff34db01fafbdfa3112e13cbd72c87
Bhavik <15388512+bhavik-knight@users.noreply.github.com15388512+bhavik-knight@users.noreply.github.com>
2023-06-06 23:41:30 -0300

    renamed firebaseConfig.js -> firebaseConfig.jsx

--- react/frontend/src/database/firebaseConfig.jsx
+++ react/frontend/src/database/firebaseConfig.jsx
@@ -2,22 +2,53 @@
 import { initializeApp } from "firebase/app";
 
 // import the firestore object from the module
-import { getFireStore } from "@firebase/firestore"
+import { getFirestore, getDocs, collection } from "firebase/firestore"
+import { getStorage } from "firebase/storage";
+
 
 // Your web app's Firebase configuration
+// const firebaseConfig = {
+//     apiKey: import.meta.env.FIREBASE_API_KEY,
+//     authDomain: import.meta.env.FIREBASE_AUTH_DOMAIN,
+//     projectId: import.meta.env.FIREBASE_PROJECT_ID,
+//     storageBucket: import.meta.env.FIREBASE_STORAGE_BUCKET,
+//     messagingSenderId: import.meta.env.FIREBASE_MESSAGING_SENDER_ID,
+//     appId: import.meta.env.FIREBASE_APP_ID
+// };
 const firebaseConfig = {
-    apiKey: process.env.FIREBASE_API_KEY,
-    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.FIREBASE_PROJECT_ID,
-    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.FIREBASE_APP_ID
+    apiKey: "AIzaSyCDaWY8_oXp4sYh9Epco9F2xhTxoR4YMZ4",
+    authDomain: "portfolio-database-9feb8.firebaseapp.com",
+    projectId: "portfolio-database-9feb8",
+    storageBucket: "portfolio-database-9feb8.appspot.com",
+    messagingSenderId: "856075319505",
+    appId: "1:856075319505:web:0832092c2939f874132df0"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig)
 
-// instantiate the firestore object within our app
-const fireStore = getFireStore(app)
+// instantiate the firestore service within our app
+const db = getFirestore(app)
 
-export { fireStore }
+// instantiate the storage (for files) services within the app
+const storage = getStorage(app)
+
+const getData = async (pageName) => {
+    try {
+        const querySnapshot = await getDocs(collection(db, pageName))
+        let newData = querySnapshot.docs.map(doc => {
+            return {
+                id: doc.id,
+                ...doc.data()
+            }
+        })
+
+        // return this newly created object on success
+        return newData
+    } catch (err) {
+        // throw error otherwise
+        throw err
+    }
+}
+
+export { db, storage, getData }
